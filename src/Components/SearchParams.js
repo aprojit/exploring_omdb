ffbd47b0bbb012ca1ca45a6bc6b18c48a6a15f94
Aprojit <aprojit@dohatec.com.bd>
2019-04-07 13:27:50 +0600

    prettier added

--- src/Components/SearchParams.js
+++ src/Components/SearchParams.js
@@ -1,52 +1,51 @@
-import React, { Component } from 'react';
-import {Consumer} from "./SearchContext"
+import React, { Component } from "react";
+import { Consumer } from "./SearchContext";
 
 class SearchParams extends Component {
-    
-    render() { 
-        return ( 
-          <Consumer>
-            {context =>(              
-              <div className="search-params">
-                <label htmlFor="title">
-                  Title
-                  <input
-                    id="title"
-                    name="title"
-                    onChange={context.handleTitleChange}
-                    value={context.title}
-                    placeholder="Title"                    
-                  />
-                </label>
+  render() {
+    return (
+      <Consumer>
+        {context => (
+          <div className="search-params">
+            <label htmlFor="title">
+              Title
+              <input
+                id="title"
+                name="title"
+                onChange={context.handleTitleChange}
+                value={context.title}
+                placeholder="Title"
+              />
+            </label>
 
-                <label htmlFor="type">
-                  Type
-                  <select
-                    id="type"
-                    name="type"
-                    value={context.type}
-                    onChange={context.handleTypeChange}
-                  >
-                    <option value="">--Select--</option>
-                    {context.types.map(type => (
-                      <option key={type} value={type}>
-                        {type}
-                      </option>
-                    ))}
-                  </select>
-                </label>
+            <label htmlFor="type">
+              Type
+              <select
+                id="type"
+                name="type"
+                value={context.type}
+                onChange={context.handleTypeChange}
+              >
+                <option value="">--Select--</option>
+                {context.types.map(type => (
+                  <option key={type} value={type}>
+                    {type}
+                  </option>
+                ))}
+              </select>
+            </label>
 
-                <button
-                  style={{ float: "left", marginBottom: 30 }}
-                  onClick={context.handleSearch}
-                >
-                  Search
-                </button>
-              </div>
-            ) }
-        </Consumer>
-         );
-    }
+            <button
+              style={{ float: "left", marginBottom: 30 }}
+              onClick={context.handleSearch}
+            >
+              Search
+            </button>
+          </div>
+        )}
+      </Consumer>
+    );
+  }
 }
- 
+
 export default SearchParams;
